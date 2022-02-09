76fb9cea0051cca1ea3b193c6a6f6d3b5453e85a
Vansh Gupta <97226346+goyalvansh4@users.noreply.github.com>
2022-02-09 20:58:11 +0530

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class hero{
+
+public:
+
+int health;
+char level;
+
+};
+
+int main(){
+
+hero ramesh;
+
+ramesh.health=70;
+ramesh.level='A';
+
+cout<<ramesh.health<<endl;
+cout<<ramesh.level<<endl;
+
+}
